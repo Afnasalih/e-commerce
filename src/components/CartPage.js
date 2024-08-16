c45991c12f8b9e66df0cae57dc205fa0956d6ec1
Afnasalih <91935925+Afnasalih@users.noreply.github.com>
2024-08-16 15:44:17 +0530

    Update CartPage.js

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -115,7 +115,7 @@ const CartPage = ({ cart, setCart }) => {
               </div>
               <p className='text-sm'>apply discount coupons</p>
             </div>
-            <Link href="/login">
+            <Link href="/checkout">
             <button
               // onClick={handleCheckout}
               className="bg-green-500 text-white px-4 py-2 rounded mt-4 hover:bg-green-600"
